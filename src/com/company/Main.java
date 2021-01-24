3ca36ce1ad55e813bd4d48d3b84e51d7f7fa7def
Agbesi Kwaku Amenyo <77252687+a-amenyo@users.noreply.github.com>
2021-01-24 17:37:25 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//
-//        Person person = new Person();
-//        person.setFirstName(" ");
-//        person.setLastName(" ");
-//        person.setAge(10);
-//        System.out.println("fullName= " + person.getFullName());
-//        System.out.println("teen= " + person.isTeen());
-//
-//
-//        person.setFirstName("John" );
-//        person.setAge(18);
-//        System.out.println("fullName " + person.getFullName());
-//        System.out.println("teen= "+ person.isTeen());
-//
-//        person.setLastName("Smith");
-//        System.out.println("fullName= " + person.getFullName());
 
-        SimpleCalculator calculator = new SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
+       Person person = new Person();
+       person.setFirstName(" ");
+       person.setLastName(" ");
+       person.setAge(10);
+       System.out.println("fullName= " + person.getFullName());
+       System.out.println("teen= " + person.isTeen());
 
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("division= " + calculator.getDivisionResult());
+
+       person.setFirstName("John" );
+       person.setAge(18);
+       System.out.println("fullName " + person.getFullName());
+       System.out.println("teen= "+ person.isTeen());
+
+       person.setLastName("Smith");
+       System.out.println("fullName= " + person.getFullName());
+
+//         SimpleCalculator calculator = new SimpleCalculator();
+//         calculator.setFirstNumber(5.0);
+//         calculator.setSecondNumber(4);
+//         System.out.println("add= " + calculator.getAdditionResult());
+//         System.out.println("subtract= " + calculator.getSubtractionResult());
+
+//         calculator.setFirstNumber(5.25);
+//         calculator.setSecondNumber(0);
+//         System.out.println("multiply= " + calculator.getMultiplicationResult());
+//         System.out.println("division= " + calculator.getDivisionResult());
 
     }
 }
+//Agbesi.Amenyo
