8239428f6fa6a3ef05730644d3a0e29e58088933
a-amenyo <agbesi.amenyo@amalitech.org>
2021-01-24 14:32:24 +0000

    Commit

---
+++ src/com/company/SimpleCalculator.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+   public double getAdditionResult (){
+
+       return this.firstNumber + this.secondNumber;
+    }
+
+    public  double getSubtractionResult ( ){
+
+        return this.firstNumber - this.secondNumber;
+    }
+
+   public double getMultiplicationResult ( ){
+
+        return (this.firstNumber * this.secondNumber);
+    }
+
+   public double getDivisionResult ( ){
+        if ( this.secondNumber==0 ){
+            return 0;
+        }
+        else return (this.firstNumber/this.secondNumber);
+    }
+
+    public double getFirstNumber(double firstNumber) {
+
+       return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+
+       this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber(double secondNumber) {
+
+       return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+
+       this.secondNumber = secondNumber;
+    }
+}
